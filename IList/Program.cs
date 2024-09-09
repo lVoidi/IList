e7a86e431d82e5c43e97dd57cd6a62398679782c
RODRIGO ARCE BASTOS <r.arce.1@estudiantec.cr>
2024-09-08 19:40:00 -0600

    Agregar archivos de proyecto.

---
+++ IList/Program.cs
@@ -0,0 +1,29 @@
+﻿
+public class IList
+{
+
+    public static void Invert()
+    {
+
+    }
+
+    public static void MergeSort()
+    {
+
+    }
+
+    public static void GetMiddle()
+    {
+
+    }
+
+    private void InsertInOrder()
+    {
+
+    }
+
+    private void DeleteFirst()
+    {
+
+    }
+}
