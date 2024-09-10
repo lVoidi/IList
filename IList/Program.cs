fb3614e4d10af0b78650d9531739a260f8df1e54
яσ∂яι <roarba011@gmail.com>
2024-09-09 19:39:20 -0600

    Programa terminado, faltan pruebas unitarias

--- IList/Program.cs
+++ IList/Program.cs
@@ -11,11 +11,20 @@ public class IList
     public static void Main(string[] args)
     {
         DoublyLinkedList<int> listA = new DoublyLinkedList<int>();
-        listA.BuildFromArray(new int[] {  3, 4, 5, 1, 2, });
+        listA.Insert(1);
+        listA.Insert(3);
+        listA.Insert(5);
+        listA.Insert(7);
+        listA.Insert(9);
 
-        DoublyLinkedList<int> listB = Invert(listA);
+        DoublyLinkedList<int> listB = new DoublyLinkedList<int>();
+        listB.Insert(2);
+        listB.Insert(4);
+        listB.Insert(6);
+        listB.Insert(8);
 
-        listA.Print();
+        DoublyLinkedList<int> listC = MergeSort(listA, listB, SortDirection.Ascending);
+        listC.Print();
     }
 
     public static DoublyLinkedList<int> Invert(DoublyLinkedList<int> List)
@@ -36,279 +45,171 @@ public class IList
 
     public static DoublyLinkedList<int> MergeSort(DoublyLinkedList<int> ListA, DoublyLinkedList<int> ListB, SortDirection direction)
     {
-        return null;
+        if (direction == SortDirection.Ascending)
+        {
+            ListA = MergeSortAscending(ListA, ListB);
+        }
+        else
+        {
+            ListA = MergeSortDescending(ListA, ListB);
+        }
+
+        return ListA;
     }
 
-    public static void GetMiddle()
+    public static DoublyLinkedList<int> MergeSortAscending(DoublyLinkedList<int> ListA, DoublyLinkedList<int> ListB)
     {
+        DoublyLinkedList<int> ListC = new DoublyLinkedList<int>();
+        Node<int> CurrentA = ListA.Head;
+        Node<int> CurrentB = ListB.Head;
 
+        while (CurrentA != null && CurrentB != null)
+        {
+            if (CurrentA.Value < CurrentB.Value)
+            {
+                ListC.Insert(CurrentA.Value);
+                CurrentA = CurrentA.Next;
+            }
+            else
+            {
+                ListC.Insert(CurrentB.Value);
+                CurrentB = CurrentB.Next;
+            }
+        }
+
+        while (CurrentA != null)
+        {
+            ListC.Insert(CurrentA.Value);
+            CurrentA = CurrentA.Next;
+        }
+
+        while (CurrentB != null)
+        {
+            ListC.Insert(CurrentB.Value);
+            CurrentB = CurrentB.Next;
+        }
+
+        return ListC;
     }
 
-    private void InsertInOrder()
+    public static DoublyLinkedList<int> MergeSortDescending(DoublyLinkedList<int> ListA, DoublyLinkedList<int> ListB)
     {
+        DoublyLinkedList<int> ListC = new DoublyLinkedList<int>();
+        Node<int> CurrentA = ListA.Head;
+        Node<int> CurrentB = ListB.Head;
 
+        while (CurrentA != null && CurrentB != null)
+        {
+            if (CurrentA.Value > CurrentB.Value)
+            {
+                ListC.Insert(CurrentA.Value);
+                CurrentA = CurrentA.Next;
+            }
+            else
+            {
+                ListC.Insert(CurrentB.Value);
+                CurrentB = CurrentB.Next;
+            }
+        }
+
+        while (CurrentA != null)
+        {
+            ListC.Insert(CurrentA.Value);
+            CurrentA = CurrentA.Next;
+        }
+
+        while (CurrentB != null)
+        {
+            ListC.Insert(CurrentB.Value);
+            CurrentB = CurrentB.Next;
+        }
+
+        return ListC;
     }
 
-    private void DeleteFirst()
+    public static int GetMiddle(DoublyLinkedList<int> List)
     {
-
+        return List.Middle.Value;
     }
+
 }
 
 public class DoublyLinkedList<T>
 {
     public Node<T> Head;
     public Node<T> Tail;
-    public int Count;
-
-    public DoublyLinkedList()
+    public Node<T> Middle;
+    public int Count = 0;
+    public void Insert(T Value)
     {
-        Head = null;
-        Tail = null;
-        Count = 0;
-    }
-
-    public void BuildFromArray(T[] array)
-    {
-        foreach (T value in array)
-        {
-            AddLast(value);
-        }
-    }
-
-    public void AddFirst(T value)
-    {
-        Node<T> node = new Node<T>
-        {
-            Value = value,
-            Next = Head,
-            Previous = null
-        };
-
-        if (Head != null)
-        {
-            Head.Previous = node;
-        }
-        else
-        {
-            Tail = node;
-        }
-
-        Head = node;
-        Count++;
-    }
-
-    public void AddLast(T value)
-    {
-        Node<T> node = new Node<T>
-        {
-            Value = value,
-            Next = null,
-            Previous = Tail
-        };
-
-        if (Tail != null)
-        {
-            Tail.Next = node;
-        }
-        else
-        {
-            Head = node;
-        }
-
-        Tail = node;
-        Count++;
-    }
-
-    public void AddAfter(Node<T> node, T value)
-    {
-        if (node == null)
-        {
-            throw new ArgumentNullException(nameof(node));
-        }
-
-        Node<T> newNode = new Node<T>
-        {
-            Value = value,
-            Next = node.Next,
-            Previous = node
-        };
-
-        node.Next = newNode;
-
-        if (newNode.Next != null)
-        {
-            newNode.Next.Previous = newNode;
-        }
-        else
-        {
-            Tail = newNode;
-        }
-
-        Count++;
-    }
-
-    public void AddBefore(Node<T> node, T value)
-    {
-        if (node == null)
-        {
-            throw new ArgumentNullException(nameof(node));
-        }
-
-        Node<T> newNode = new Node<T>
-        {
-            Value = value,
-            Next = node,
-            Previous = node.Previous
-        };
-
-        node.Previous = newNode;
-
-        if (newNode.Previous != null)
-        {
-            newNode.Previous.Next = newNode;
-        }
-        else
-        {
-            Head = newNode;
-        }
-
-        Count++;
-    }
-
-    public void RemoveFirst()
-    {
-        if (Head == null)
-        {
-            throw new InvalidOperationException("The list is empty");
-        }
-
-        Head = Head.Next;
-        Count--;
+        Node<T> NewNode = new Node<T>();
+        NewNode.Value = Value;
 
         if (Head == null)
         {
-            Tail = null;
+            Head = NewNode;
+            Tail = NewNode;
+            Middle = NewNode;
         }
         else
         {
-            Head.Previous = null;
-        }
-    }
-
-    public void RemoveLast()
-    {
-        if (Tail == null)
-        {
-            throw new InvalidOperationException("The list is empty");
-        }
-
-        Tail = Tail.Previous;
-        Count--;
-
-        if (Tail == null)
-        {
-            Head = null;
-        }
-        else
-        {
-            Tail.Next = null;
-        }
-    }
-
-    public void Remove(Node<T> node)
-    {
-        if (node == null)
-        {
-            throw new ArgumentNullException(nameof(node));
-        }
-
-        if (node.Previous != null)
-        {
-            node.Previous.Next = node.Next;
-        }
-        else
-        {
-            Head = node.Next;
-        }
-
-        if (node.Next != null)
-        {
-            node.Next.Previous = node.Previous;
-        }
-        else
-        {
-            Tail = node.Previous;
-        }
-
-        Count--;
-    }
-
-    public void Clear()
-    {
-        Head = null;
-        Tail = null;
-        Count = 0;
-    }
-
-    public Node<T> Find(T value)
-    {
-        Node<T> current = Head;
-
-        while (current != null)
-        {
-            if (current.Value.Equals(value))
+            Tail.Next = NewNode;
+            NewNode.Previous = Tail;
+            Tail = NewNode;
+            // Set middle without iterating the list
+            if (Count % 2 == 0)
             {
-                return current;
+                Middle = Middle.Next;
             }
 
-            current = current.Next;
         }
-
-        return null;
+        Count++;
     }
 
-    public bool Contains(T value)
+    public T DeleteFirst()
     {
-        return Find(value) != null;
+        // Deletes the first element of the doubly linked list and returns it
+        if (Head == null)
+        {
+            return default(T);
+        }
+        else
+        {
+            Node<T> Aux = Head;
+            Head = Head.Next;
+            Head.Previous = null;
+            Count--;
+            return Aux.Value;
+        }
+    }
+
+    public T DeleteLast()
+    {
+        // Deletes the last element of the doubly linked list and returns it
+        if (Tail == null)
+        {
+            return default(T);
+        }
+        else
+        {
+            Node<T> Aux = Tail;
+            Tail = Tail.Previous;
+            Tail.Next = null;
+            Count--;
+            return Aux.Value;
+        }
     }
 
     public void Print()
     {
-        Node<T> current = Head;
-
-        while (current != null)
+        Node<T> Current = Head;
+        while (Current != null)
         {
-            Console.WriteLine(current.Value);
-            current = current.Next;
+            Console.WriteLine(Current.Value);
+            Current = Current.Next;
         }
     }
 
-    public void PrintReverse()
-    {
-        Node<T> current = Tail;
-
-        while (current != null)
-        {
-            Console.WriteLine(current.Value);
-            current = current.Previous;
-        }
-    }
-
-    public T[] ReturnLinkedListAsArray()
-    {
-        T[] array = new T[Count];
-        Node<T> current = Head;
-        int index = 0;
-
-        while (current != null)
-        {
-            array[index++] = current.Value;
-            current = current.Next;
-        }
-
-        return array;
-    }
 }
 
 public class Node<T>
